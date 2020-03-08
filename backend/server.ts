3e8a02da493cd7d4cb705fd6f9ac2cd87c2c7ce9
Jeffrey Parrish (AMY PC) <jeffs.linux@gmail.com>
2020-03-07 19:21:43 -0500

    fixing backend system, adding package commands, testing and getting jwt login system working (it works!)

--- backend/server.ts
+++ backend/server.ts
@@ -5,19 +5,32 @@ import jwt from "jsonwebtoken";
 const secret = "temporary-make-me-configurable";
 const app = express();
 app.use(expressJWT({secret}).unless({path:["/api/login"]}));
-app.post("/api/login",function(req,res){
-	const [username,password] = [req.params.username || "", req.params.password || ""];
-	if(username.length === 0 || password.length === 0){
-		res.status(401).send(JSON.stringify({error: ""}));
-		return;
+app.use(express.json());
+
+app.post("/api/login", function(req,res){
+	try{
+		const {username, password} = req.body;
+		if(username.length === 0 || password.length === 0) {
+			res.status(401).send(JSON.stringify({error: "Username or password was empty"}));
+			return;
+		}
+		if(username === "jcapinc" && password === "hercules1"){
+			res.send(jwt.sign({
+				username: "jcapinc",
+				id:"1"
+			}, secret));
+			return;
+		}
+	} catch(exception){
+		res.status(401).send(JSON.stringify({error: exception.toString()}));
 	}
-	if(username === "jcapinc" && password === "hercules1"){
-		res.send(jwt.sign({
-			username: "jcapinc",
-			id:"1"
-		}, secret));
-		return;
-	}
-	res.send(401).send(JSON.stringify({error: "incorrect username or password"}));
+	res.status(401).send(JSON.stringify({error: "incorrect username or password"}));
 });
-app.listen(9001)
+
+app.get("/api/status", function(_,res){
+	res.send(JSON.stringify({loggedin:true}));
+});
+
+const port = 9001;
+app.listen(port);
+console.log("listening on port 9001");
