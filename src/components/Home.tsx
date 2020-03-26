bc8d0de9a04df96d82137c5353b114dcd39d1845
Jeffrey Parrish (AMY PC) <jeffs.linux@gmail.com>
2020-03-26 18:30:46 -0400

    removing unecessary imports from home and updating model types

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { AppState } from '../store';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import * as S from 'semantic-ui-react';
 
 export function Home(){
 	//const requests = useSelector((state: AppState) => state.currentState.requests);
+	return <HomeMarkup />
+}
+
+export interface IHomeMarkupProps{}
+
+export function HomeMarkup(props: IHomeMarkupProps) {
 	return <div style={{textAlign:"center"}}>
-		<S.Message  title="There are no prayer requests">
-			Create a new prayer request to get started.
-		</S.Message>}
-	</div>;
+		
+	<S.Message>
+		<S.Message.Header>There are No Families</S.Message.Header>
+		Create a new prayer request to get started.
+	</S.Message>}
+</div>;
 }
