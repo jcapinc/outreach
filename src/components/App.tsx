bd4d03ea4338acb29e672ab77d075925e919df8a
Jeffrey Parrish (AMY PC) <jeffs.linux@gmail.com>
2020-03-28 09:42:55 -0400

    adding breadcrumb functionality for members, I should probably abstract the breadcrumb functionality instead

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,6 +7,7 @@ import { AppState} from '../store';
 import { Login } from './Login';
 import { FamilyForm } from './Families';
 import * as S from 'semantic-ui-react';
+import { IPerson } from '../../ModelTypes';
 
 
 export function App(){
@@ -16,6 +17,7 @@ export function App(){
 			<Navigation />
 			<Switch>
 				<Route exact path="/" component={Home} />
+				<Route path="/family/:id/member/:memberid" component={FamilyRoute} />
 				<Route path="/family/:id" component={FamilyRoute} />
 			</Switch>
 		</React.Fragment> : 
@@ -27,14 +29,57 @@ export function FamilyRoute(){
 	let {id} = useParams();
 	const surname = useSelector((state: AppState) => state.currentState.families.find(rec => rec.guid === id)?.surname || "");
 	return <React.Fragment>
-		<div>
-			<S.Breadcrumb>
-				<S.Breadcrumb.Section><Link to="/">Home</Link></S.Breadcrumb.Section>
-				<S.Breadcrumb.Divider />
-				<S.Breadcrumb.Section><Link to={"/family/" + id}>{surname} Family</Link></S.Breadcrumb.Section>
-			</S.Breadcrumb>
-			<hr />
-		</div>
+		
+			<FamilyBreadCrumb familyid={id} familysurname={surname} />
+		
 		<FamilyForm id={id || ""} />
+	</React.Fragment>;
+}
+
+export function MemberRoute(){
+	let {id,memberid} = useParams();
+	const [surname,membername] = useSelector((state: AppState) => {
+		const family = state.currentState.families.find(fam => fam.guid === id);
+		if(family === undefined) return ["Unknown","Unknown"];
+		const member = family.members.find(member => member.guid === memberid);
+		return [family.surname,`${member?.firstname} ${member?.lastname}`];
+	});
+	return <React.Fragment>
+		<FamilyBreadCrumb familyid={id} familysurname={surname}>
+			<MemberBreadCrumb familyid={id} membername={membername} memberid={memberid} />
+		</FamilyBreadCrumb>
+	</React.Fragment>;
+}
+
+export interface IFamilyBreadCrumbProps{
+	children?: React.ReactNode;
+	familyid: string | undefined;
+	familysurname: string | undefined;
+}
+
+export function FamilyBreadCrumb({children, familyid, familysurname}:IFamilyBreadCrumbProps){
+	return <div>
+		<S.Breadcrumb>
+			<S.Breadcrumb.Section><Link to="/">Home</Link></S.Breadcrumb.Section>
+			<S.Breadcrumb.Divider />
+			<S.Breadcrumb.Section><Link to={"/family/" + familyid}>{familysurname} Family</Link></S.Breadcrumb.Section>
+			{children || ""}
+		</S.Breadcrumb>
+		<hr />
+	</div>
+	;
+}
+
+export interface IMemberBreadCrumbProps{
+	familyid: string | undefined;
+	membername: string | undefined;
+	memberid: string | undefined;
+}
+
+export function MemberBreadCrumb({familyid, membername, memberid}: IMemberBreadCrumbProps){
+	const link = `/family/${familyid}/member/${memberid || ""}`;
+	return <React.Fragment>
+		<S.Breadcrumb.Divider />
+		<S.Breadcrumb.Section><Link to={link}>{membername}</Link></S.Breadcrumb.Section>
 	</React.Fragment>;
 }
